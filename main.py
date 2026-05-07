7933492d808ede0c7f73a37efd81e3093d828da3
Sunny Sun <chs019@ucsd.edu>
2026-05-06 21:45:32 -0700

    Fix lint errors

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
-import os
-import sys
-
-x=1+2
-
-def bad_function():
-    print("hello")
-
 def main():
+    x = 1+2
+    print(x)
     print("Hello from dsc190-a05!")
-
 
 if __name__ == "__main__":
     main()
